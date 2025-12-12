6ac2e504e49976111628c644f83bfd7d6d8e5fc5
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2025-12-12 23:11:56 +0530

    Time: 0 ms (100%), Space: 9.1 MB (85.35%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        int arr [13]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string romans[13]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        string ans="";
+
+        while(num>0){
+
+            for(int i=0; i<13; i++){
+                if(num>=arr[i]){
+                    num-=arr[i];
+                    ans+=romans[i];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+};
+
+
+
+//here we have used the brute force approch to check the elements of string and make the input by comparing to romans in loops
+//date- 12/12/25
+//Prathmesh
