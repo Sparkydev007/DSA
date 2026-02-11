eeaba2b8b921c248592dd74e2e0b11c20e0f7c46
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-02-11 23:34:51 +0530

    Time: 4 ms (11.75%), Space: 23.7 MB (98.1%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,19 +2,18 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int insertPosition = 0;
-        for (int i = 0; i < nums.size(); i++) {
+
+        for (int i = 0; i< nums.size(); i++) {
             if (nums[i] != 0) {
-                nums[insertPosition] = nums[i];
+                nums [insertPosition] = nums[i];
                 insertPosition++;
             }
+
         }
+
 
         while (insertPosition < nums.size()) {
             nums[insertPosition++] = 0;
         }
     }
 };
-
-
-
-// here we have used the approch which involed the sorting the int array in zero & Non-zero while only taking the non zero elements into the initilized array and pushing all the zero arrays to the very end while keeping the complexity O(1)
