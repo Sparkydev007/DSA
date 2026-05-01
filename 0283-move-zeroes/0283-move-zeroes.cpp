403f20ef55eaff94e2440a9d6a993693536a8e79
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-05-01 14:07:19 +0530

    Time: 0 ms (100%), Space: 23.8 MB (81.84%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j=0;
-        int n = nums.size();
-        for(int i=0;i<n;i++)
+        int j = 0;   //pointer 1 initialized 
+        int i = nums.size(); // pointer 2 initialize (incremental)
+
+        for (int i = 0; i< nums.size();i++)
         {
             if(nums[i]!=0)
-        {
-            swap (nums[i], nums[j]);
-            j++;
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        }
+
+
         
     }
 };
