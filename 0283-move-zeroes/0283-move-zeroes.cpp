be929a22cbab5a94b4d01f28e1c4a1528f0f174b
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-05-05 00:17:39 +0530

    Time: 0 ms (100%), Space: 23.7 MB (98.51%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,14 +4,12 @@ public:
         int j = 0;
         int i = nums.size();
 
-        for(i=0;i<nums.size();i++){
+        for (i=0;i<nums.size();i++){
             if (nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
             }
         }
+        
     }
 };
-
-
-//we have used 2 pointers here
