7ce10762bb2e780884f8965fcacc3f52152f6c0b
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-05-05 00:04:22 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.85%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,11 +4,12 @@ public:
         int j = 0;
         int i = nums.size();
         for (i=0;i<nums.size();i++){
-            if(nums[i]!= 0){
-                swap(nums[i],nums[j]);
+            if (nums[i]!=0){
+                swap (nums[i], nums[j]);
                 j++;
             }
         }
-        
+
+
     }
 };
