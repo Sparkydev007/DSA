022eed560a7b0eb4504d031c5593fa81129f270e
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-05-06 23:31:43 +0530

    Time: 0 ms (100%), Space: 23.8 MB (54.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,15 +3,12 @@ public:
     void moveZeroes(vector<int>& nums) {
         int j = 0;
         int i = nums.size();
-
         for (i=0;i<nums.size();i++){
-            if (nums[i]!=0){
+            if(nums[i]!=0){
                 swap(nums[i],nums[j]);
                 j++;
             }
         }
-        
-
         
     }
 };
