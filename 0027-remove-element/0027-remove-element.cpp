071b8192360ad76ad656bcd55d63532fd275fd01
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-06-19 22:40:29 +0530

    Time: 0 ms (100%), Space: 12 MB (11.36%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        
+
+        vector<int> temp;
+        for( int i : nums){
+            if (i!=val)  {
+                temp.push_back(i);
+            }
+        }
+        nums = temp;
+        return nums.size();
+    }
+};
