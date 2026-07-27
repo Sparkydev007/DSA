29ae0de312f9fa6e776828580b394651ac582b54
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-07-27 17:02:04 +0530

    Time: 4 ms (18.79%), Space: 28.4 MB (12.2%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> counter;
-        int treshold = nums.size()/2;
-        for (int num : nums){
-            counter[num]++;
-
-            if(counter[num]>treshold) {
-                return num;
+        unordered_map<int, int> counts;
+        int i = 0;
+        for(i=0;i<nums.size();i++){
+            counts[nums[i]]++;
+            if(counts[nums[i]]> nums.size() / 2){
+                return nums[i];
             }
         }
         return -1;
