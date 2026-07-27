9430de50b63f236d6fc584e7d766032cda4804d3
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-07-27 16:19:39 +0530

    Time: 0 ms (100%), Space: 23.8 MB (83.36%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
-        for (int i =0; i<nums.size();i++)
-        if (nums[i] != 0){
-            swap(nums[i],nums[j]);
-            j++;
+        int i = 0;
+        int  j = 0;
+
+        for (i=0;i<nums.size();i++){
+            if(nums[i] != 0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
-        return;
     }
 };
-
-
-//the first self solved leetcode question!!!!!!!!!!!!!!!!!!!!!!!! 24-07-26!
