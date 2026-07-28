5c0ead7871c683f67f545a5700b7693c881adb71
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-07-28 10:58:11 +0530

    Time: 0 ms (100%), Space: 29.4 MB (94.86%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
-        k = k%n;
-        reverse(nums.begin(),nums.begin()+(n-k));
-        reverse(nums.begin()+(n-k),nums.end());
-        reverse(nums.begin(),nums.end());
+        int n = nums.size();  //intiaize the the n as size of nums
+        k = k%n; // formula to rotate only needed part not whole array 
+
+        reverse(nums.begin(),nums.begin()+(n-k)); //cpp reverse funtion initialse from start to full array including (n-k)  
+        reverse(nums.begin()+(n-k),nums.end()); // again reverse array but this time k steps stays difrrenrt
+        reverse(nums.begin(),nums.end()); //reverse all array 
+
     }
 };
