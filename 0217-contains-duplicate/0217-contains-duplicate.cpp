1873f8a660492e2aa1a325563d800f2a56209823
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-08-07 23:48:32 +0530

    Time: 66 ms (54.42%), Space: 111.3 MB (43.23%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        std::unordered_set<int> set;
-        
-        for (int num : nums) {
-            // If the number is already in our set, we found a duplicate
-            if (set.contains(num)) {
+        unordered_set<int> set;
+
+        for(int num : nums){
+            if(set.contains(num)){
                 return true;
+            
             }
-            // Otherwise, add it to the set to keep track of it
+
             set.insert(num);
         }
-        
-        // If we check everything without returning true, all elements are distinct
-        return false;
+           return false;
     }
 };
