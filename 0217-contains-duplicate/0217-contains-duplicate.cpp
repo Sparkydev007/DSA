a5d506159628dc87da68e2a6edb1e75b7de9f471
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-08-11 15:44:38 +0530

    Time: 64 ms (59.24%), Space: 111.3 MB (47.03%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_set <int> set;
+        std::unordered_set<int>tab;
 
-        for( int num : nums){
-            if (set.contains(num)){
-               return true;
+        for(int num : nums){
+            if (tab.count(num)){
+                return true;
             }
 
-            set.insert(num);
+            tab.insert(num);
         }
 
         return false;
-    }  
+    }
 };
