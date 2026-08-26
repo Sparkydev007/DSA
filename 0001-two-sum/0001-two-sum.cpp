e7249bdec1a080992b2687d58c32dee203a3d95b
Prathmesh Bunde <122979844+Sparkydev007@users.noreply.github.com>
2026-08-26 22:46:51 +0530

    Time: 0 ms (100%), Space: 14.9 MB (42.97%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,15 +6,15 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             int complement = target - nums[i];
             
-            // Check if the required complement already exists in the map
+            
             if (num_to_index.count(complement)) {
                 return {num_to_index[complement], i};
             }
             
-            // Store the index of the current number
+            
             num_to_index[nums[i]] = i;
         }
         
-        return {}; // Return empty vector if no solution is found
+        return {}; 
     }
-};
+};                
